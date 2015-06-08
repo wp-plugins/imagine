456c343243f6438f6ec37c0bab1293353685c76a
mmichel <mmichel@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-08 14:55:59 +0000

    0.99.8
    git-svn-id: https://plugins.svn.wordpress.org/imagine/trunk@1176773 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ templates/imagine-gallery-carousel.php
@@ -0,0 +1,45 @@
+<script src="<?php echo plugin_dir_url(__FILE__) . '../js/jquery.film_roll.js'; ?>"></script>
+<?php
+
+global $wpdb;
+
+if (isset($_GET['imagine']['gallery'])) {
+	$gallery = intval($_GET['imagine']['gallery']);
+}
+$imgs = $wpdb -> get_results("SELECT * FROM ".$wpdb->prefix."imagine_img WHERE galleryId = '$gallery'");
+$gallery = $wpdb -> get_row("SELECT * FROM ".$wpdb->prefix."imagine_gallery WHERE galleryId = '$gallery'");
+$galslug = $gallery -> gallerySlug;
+echo '<div class="imagine-carousel-wrap" template="'.esc_attr($tslug).'">';
+
+foreach ($imgs as $img) {
+	$imgid = $img -> imgId;
+	$gid = $img -> galleryId;
+	$filename = $img -> imgFilename;
+	$plugindir = wp_upload_dir();
+	$pluginurl = $plugindir['baseurl'];
+	$plugindir = $plugindir['basedir'];
+    echo '<div class="imagine-carousel-thumbnail-wrap">';
+	echo '<img src="' . $pluginurl . '/imagine/' . esc_attr($galslug) . '/thumbs/thumb_' . esc_attr($filename) . '" gid="' . esc_attr($gid) . '" imgid="' . esc_attr($imgid) . '" layovertemp="' . esc_attr($layovertemplate) . '">';
+    echo '</div>';
+}
+echo '<div class="spacer" style="clear: both;"></div>';
+echo '</div>';
+?>
+
+
+<script>
+    $(document).ready( function() {
+        var w = $('.imagine-carousel-wrap').parent('.imagine').width() - 12;
+        
+        fr = new FilmRoll({
+            container: '.imagine-carousel-wrap',
+            height: 200,
+            pager: false,
+          });
+    });
+    
+    $(window).on('resize', function() {
+        var w = $('.imagine-carousel-wrap').parent('.imagine').width() - 12;
+        
+    });
+</script>
---
+++ templates/imagine-gallery-wall.php
@@ -0,0 +1,41 @@
+<script src="<?php echo plugin_dir_url(__FILE__) . '../js/jquery.collagePlus.js'; ?>"></script>
+<?php
+
+global $wpdb;
+
+if (isset($_GET['imagine']['gallery'])) {
+	$gallery = intval($_GET['imagine']['gallery']);
+}
+$imgs = $wpdb -> get_results("SELECT * FROM ".$wpdb->prefix."imagine_img WHERE galleryId = '$gallery'");
+$gallery = $wpdb -> get_row("SELECT * FROM ".$wpdb->prefix."imagine_gallery WHERE galleryId = '$gallery'");
+$galslug = $gallery -> gallerySlug;
+echo '<div class="imagine-wall-wrap">';
+
+foreach ($imgs as $img) {
+	$imgid = $img -> imgId;
+	$gid = $img -> galleryId;
+	$filename = $img -> imgFilename;
+	$plugindir = wp_upload_dir();
+	$pluginurl = $plugindir['baseurl'];
+	$plugindir = $plugindir['basedir'];
+	echo '<img template="'.esc_attr($tslug).'" class="imagine-thumbnail-wrap" src="' . $pluginurl . '/imagine/' . esc_attr($galslug) . '/thumbs/thumb_' . esc_attr($filename) . '" gid="' . esc_attr($gid) . '" imgid="' . esc_attr($imgid) . '" layovertemp="' . esc_attr($layovertemplate) . '">';
+
+}
+echo '<div class="spacer" style="clear: both;"></div>';
+echo '</div>';
+?>
+
+
+<script>
+    $(document).ready( function() {
+        var w = $('.imagine-wall-wrap').parent('.imagine').width() - 12;
+        
+        $('.imagine-wall-wrap').css({width: w, padding: '10px'}).collagePlus({'targetHeight': 200});
+    });
+    
+    $(window).on('resize', function() {
+        var w = $('.imagine-wall-wrap').parent('.imagine').width() - 12;
+        
+        $('.imagine-wall-wrap').css({width: w, padding: '10px'}).collagePlus({'targetHeight': 200});
+    });
+</script>
